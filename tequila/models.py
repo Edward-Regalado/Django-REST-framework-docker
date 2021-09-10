e7ca685893e0c600d9ccca79a800f605c94a1060
Edward-Regalado <tregalado30@gmail.com>
2021-09-09 23:33:54 -0700

    started django project, first commit

---
+++ tequila/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.deletion import CASCADE
+
+# Create your models here.
+
+class Tequila(models.Model):
+    brand = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    body = models.TextField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.title
+
+    
